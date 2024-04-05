f16c7f6b59e3105b37473e8f16e6424e066a3833
Naveed Akhtar <naveed.akhtar@ketch.com>
2024-04-05 20:40:17 +0530

    chore: updated template

--- template.js
+++ template.js
@@ -1,76 +1,102 @@
-(function myCustomConsentIntegration (window) {
+;(function myCustomConsentIntegration (window) {
   window.tealiumCmpIntegration = window.tealiumCmpIntegration || {}
+
   window.tealiumCmpIntegration.cmpName = 'Ketch'
-  window.tealiumCmpIntegration.cmpIntegrationVersion = 'v0.0.1'
+  window.tealiumCmpIntegration.cmpIntegrationVersion = 'v1.0.0'
   var disableVendorIdValidation = false;
-  
-  function cmpFetchCurrentLookupKey () {
-    if (disableVendorIdValidation) {
-      return (window.tealiumCmpIntegration && window.tealiumCmpIntegration.map && Object.keys(window.tealiumCmpIntegration.map)[0]) || "(Vendor ID check disabled)";
-    }
-    if(!_config) return '';
-    var id = _config.deployment.version;
-    return id || '';
+
+  window.tealiumCmpIntegration.cmpFetchCurrentConsentDecision = cmpFetchCurrentConsentDecision
+  window.tealiumCmpIntegration.cmpFetchCurrentLookupKey = cmpFetchCurrentLookupKey
+  window.tealiumCmpIntegration.cmpCheckIfOptInModel = cmpCheckIfOptInModel
+  window.tealiumCmpIntegration.cmpCheckForWellFormedDecision = cmpCheckForWellFormedDecision
+  window.tealiumCmpIntegration.cmpCheckForExplicitConsentDecision = cmpCheckForExplicitConsentDecision
+  window.tealiumCmpIntegration.cmpCheckForTiqConsent = cmpCheckForTiqConsent
+  window.tealiumCmpIntegration.cmpConvertResponseToGroupList = cmpConvertResponseToGroupList
+
+ function cmpCheckIfOptInModel () {
+    return true;
   }
 
   function cmpFetchCurrentConsentDecision () {
-    if (!window._consent_choices || !window._config) {
-      return false; 
+    // default consent
+    var getDefaultConsent = function () {
+      return {
+        analytics: true,
+        advertising: true,
+        email_mktg: true,
+        essential_services: true
+      }; 
     }
-    return {'config': window._config, 'consent': window._consent_choices};
+    
+    var getCookie = function (name) {
+      const cookieValue = document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)');
+      return cookieValue ? cookieValue.pop() : '';
+    }
+
+    var decodeCookieValue = function (encodedValue) {
+      const urlDecoded = decodeURIComponent(encodedValue);
+      const base64Decoded = atob(urlDecoded);
+      return base64Decoded;
+    }
+
+    const consentCookieValue = getCookie('_ketch_consent_v1_');
+    let consent = {}
+    if (consentCookieValue) {
+      const decodedValue = decodeCookieValue(consentCookieValue);
+      const cookieConsent = JSON.parse(decodedValue);
+      for (const purposeCode in cookieConsent) {
+        const val = cookieConsent[purposeCode];
+        if (val) {
+          const status = val.status;
+          consent[purposeCode] = status === 'granted' ? true : false;
+        }
+      }
+
+    } else {
+      console.log('cmpFetchCurrentConsentDecision: setting default consent');
+      consent = getDefaultConsent()
+    }
+    return {'consent': consent};
   }
 
-  function cmpCheckIfOptInModel () {
-    var decision = cmpFetchCurrentConsentDecision();
-    if (decision && decision.config && decision.config.purposes[0].legalBasisCode == 'consent_optin') {
-      return true;
-    }
-    return false;
+  function cmpFetchCurrentLookupKey () {
+    return (window.tealiumCmpIntegration && window.tealiumCmpIntegration.map && Object.keys(window.tealiumCmpIntegration.map)[0]) || '(Vendor ID check disabled)'
   }
 
+  // Should return a boolean - true if the raw decision meets our expectations for the CMP
   function cmpCheckForWellFormedDecision (cmpRawOutput) {
     if (!cmpRawOutput) { 
       return false; 
     }
-    if (cmpRawOutput && cmpRawOutput.consent && cmpRawOutput.consent.purposes) {
+    if (cmpRawOutput && cmpRawOutput.consent) {
       return true;
     }
+   
     return false;
   }
 
   function cmpCheckForExplicitConsentDecision (cmpRawOutput) {
-    if (!cmpRawOutput || !cmpRawOutput.consent || !cmpRawOutput.consent.purposes) {
-      return false; 
-    }
-    
-    if (Object.keys(cmpRawOutput.consent.purposes).length > 0 && window._explicit_consent) {
-      return true;
-    }
-    return false;
+   return true
   }
 
-  function cmpCheckForTiqConsent (cmpRawOutput, tiqGroupName) {
-    return true;
-  }
-
+  // Should return an array of consented vendors/purposes - these should match the Purposes in Tealium iQ exactly
   function cmpConvertResponseToGroupList (cmpRawOutput) {
     var purposeArray = [];
     
-    cmpRawOutput && cmpRawOutput.consent.purposes && Object.keys(cmpRawOutput.consent.purposes).forEach(function (key) {
-      if (cmpRawOutput.consent.purposes[key] === true) {
+    cmpRawOutput && cmpRawOutput.consent && Object.keys(cmpRawOutput.consent).forEach(function (key) {
+      if (cmpRawOutput.consent[key] === true) {
         purposeArray.push(key);
       }
     });
     return purposeArray;
   }
 
-  window.tealiumCmpIntegration.cmpFetchCurrentConsentDecision = cmpFetchCurrentConsentDecision;
-  window.tealiumCmpIntegration.cmpCheckIfOptInModel = cmpCheckIfOptInModel;
-  window.tealiumCmpIntegration.cmpCheckForWellFormedDecision = cmpCheckForWellFormedDecision;
-  window.tealiumCmpIntegration.cmpCheckForExplicitConsentDecision = cmpCheckForExplicitConsentDecision;
-  window.tealiumCmpIntegration.cmpCheckForTiqConsent = cmpCheckForTiqConsent;
-  window.tealiumCmpIntegration.cmpConvertResponseToGroupList = cmpConvertResponseToGroupList;
-  window.tealiumCmpIntegration.cmpFetchCurrentLookupKey = cmpFetchCurrentLookupKey;
-  console.log('setup tealium <> ketch integration');
-  console.log(window.tealiumCmpIntegration.cmpName);
+  function cmpCheckForTiqConsent (cmpRawOutput, tiqGroupName) {
+    // treat things we don't understand as an opt-out
+    if (cmpCheckForWellFormedDecision(cmpRawOutput) !== true) return false
+
+    tiqGroupName = tiqGroupName || 'tiq-group-name-missing'
+    var allowedGroups = cmpConvertResponseToGroupList(cmpRawOutput)
+    return allowedGroups.indexOf(tiqGroupName) !== -1
+  }
 })(window);
